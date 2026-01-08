4f7c30025abd0636c4485e7deb15386a77d5043f
razherana <razherana@gmail.com>
2026-01-08 21:26:44 +0300

    initalized project

---
+++ src/main/java/mg/razherana/aizatransport/AizatransportApplication.java
@@ -0,0 +1,13 @@
+package mg.razherana.aizatransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AizatransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AizatransportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/razherana/aizatransport/AizatransportApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.razherana.aizatransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AizatransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
